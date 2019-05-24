d179535fb359ba8f00682080b3e50311c42a3798
Aisa Mukukenova <mkaysa@gmail.com>
2019-05-24 12:23:31 +0100

    added pundit and dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  include Pundit
+
+  after_action :verify_authorized, except: [ :index, :home ], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [ :index, :home ] ,  unless: :skip_pundit?
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || dashboard_path
+  end
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,21 @@
 class GamesController < ApplicationController
   def show
     # lobby
+    # anyone should be able to view/join the game
+    skip_pundit?
   end
 
   def new
     # where you create a game (click +)
+    authorise @user
   end
 
   def create
     # generate a link
+    authorise @user
   end
 
   def destroy
+    authorise @user
   end
 end
