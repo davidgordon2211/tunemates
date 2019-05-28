fd06f8a428cd06f1be4f08b6674ee8c432934d84
Aisa Mukukenova <mkaysa@gmail.com>
2019-05-28 11:55:28 +0100

    updated foreign keys for users, songs and invited users in schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :games, :foreign_key => 'host_id', class_name: "Game"
+  has_many :songs
+  has_many :user_guesses, :foreign_key => 'guesser_id', class_name: "User"
+  has_many :user_guesses, :foreign_key => 'submitter_id', class_name: "User"
 end
