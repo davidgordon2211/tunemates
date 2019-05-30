087b2ea3c1837990fcfe576c0472d9f660bd1f48
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-05-30 15:57:52 +0100

    added logo and meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,9 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 end
+
