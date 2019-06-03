4fc5532dc9cfa8c40b565bd90dbcde1c5c22886d
Aisa Mukukenova <mkaysa@gmail.com>
2019-06-03 15:48:44 +0100

    added placeholder for select2

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -6,3 +6,11 @@ const initSelect2 = () => {
 };
 
 export { initSelect2 };
+
+const placeholderSelect2 = () => {
+  $(".placeholder").select2({
+      placeholder: "Type names here"
+  });
+}
+
+export { placeholderSelect2 };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,8 @@ import "bootstrap";
 import 'select2/dist/css/select2.css';
 
 import { initSelect2 } from '../components/init_select2';
+import { placeholderSelect2 } from '../components/init_select2';
 
 initSelect2();
+placeholderSelect2();
+
