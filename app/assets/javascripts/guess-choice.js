66185515ff109bb2cfe08c21503bf627cecf485c
Kieran Doyle <kieranhbdoyle@gmail.com>
2019-06-03 09:16:59 +0100

    navbar and jquery attempts

--- app/assets/javascripts/guess-choice.js
+++ app/assets/javascripts/guess-choice.js
@@ -4,50 +4,50 @@ $(document).ready(function(){
   });
 });
 
-// $(document).ready(function(){
-//   $(".category-guess-purple").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".category-guess-purple").click(function(){
+    $(this).toggleClass("active");
+  });
+});
 
-// $(document).ready(function(){
-//   $(".category-guess-blue").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".category-guess-blue").click(function(){
+    $(this).toggleClass("active");
+  });
+});
 
-// $(document).ready(function(){
-//   $(".category-guess-yellow").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".category-guess-yellow").click(function(){
+    $(this).toggleClass("active");
+  });
+});
 
-// $(document).ready(function(){
-//   $(".in-game-player-button-red").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".in-game-player-button-red").click(function(){
+    $(this).toggleClass("active");
+  });
+});
 
-// $(document).ready(function(){
-//   $(".in-game-player-button-blue").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".in-game-player-button-blue").click(function(){
+    $(this).toggleClass("active");
+  });
+});
 
-// $(document).ready(function(){
-//   $(".in-game-player-button-yellow").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".in-game-player-button-yellow").click(function(){
+    $(this).toggleClass("active");
+  });
+});
 
-// $(document).ready(function(){
-//   $(".in-game-player-button-purple").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".in-game-player-button-purple").click(function(){
+    $(this).toggleClass("active");
+  });
+});
 
-// $(document).ready(function(){
-//   $(".in-game-player-button-green").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+$(document).ready(function(){
+  $(".in-game-player-button-green").click(function(){
+    $(this).toggleClass("active");
+  });
+});
