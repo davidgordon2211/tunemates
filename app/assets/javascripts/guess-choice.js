51f1009eb70e4d85370566ed2285626c307560ec
Kieran Doyle <kieranhbdoyle@gmail.com>
2019-06-03 13:40:00 +0100

    javascript for song selection

--- app/assets/javascripts/guess-choice.js
+++ app/assets/javascripts/guess-choice.js
@@ -1,53 +1,14 @@
-$(document).ready(function(){
-  $(".category-guess-red").click(function(){
-    $(this).toggleClass("active");
-  });
-});
+const categories = [ ".category-guess-red", ".category-guess-purple", ".category-guess-blue", ".category-guess-yellow", ".in-game-player-button-blue", ".in-game-player-button-yellow", ".in-game-player-button-purple", ".in-game-player-button-green", ".in-game-player-button-red", ]
 
-// $(document).ready(function(){
-//   $(".category-guess-purple").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+const turnActive = (categories) => {
+  categories.forEach((category) => {
+    const cards = Array.from(document.querySelectorAll(category));
+    cards.forEach((card) => {
+      card.addEventListener("click", (e) => {
+        event.currentTarget.classList.toggle("active");
+      })
+    })
+  })
+}
 
-// $(document).ready(function(){
-//   $(".category-guess-blue").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".category-guess-yellow").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".in-game-player-button-red").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".in-game-player-button-blue").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".in-game-player-button-yellow").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".in-game-player-button-purple").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
-
-// $(document).ready(function(){
-//   $(".in-game-player-button-green").click(function(){
-//     $(this).toggleClass("active");
-//   });
-// });
+turnActive(categories);
