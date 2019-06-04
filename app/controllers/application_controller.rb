4c344c2c20ab73937272437169b394d11a12c2d5
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-06-04 09:38:28 +0100

    changed profile image and slider

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo, :color])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo, :color])
   end
 
   # after_action :verify_authorized, except: [ :index, :home ], unless: :skip_pundit?
