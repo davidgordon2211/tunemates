8d3f01a915dbb30e3584181fbe46e964c475e062
Inanna Craig-Morse <inanna.craigmorse@gmail.com>
2019-06-04 17:01:01 +0100

    hjk

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,8 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
+  # process resize_to_fit: [300, 300]
+
 
   def default_url
     "https://res.cloudinary.com/dtbs7luvc/image/upload/v1559563021/noun_avatar_1901054_odfold.png"
