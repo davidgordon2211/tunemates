902eb5db3c2603ba6be0b3b139072ae8ae578c8d
Aisa Mukukenova <mkaysa@gmail.com>
2019-06-05 16:17:40 +0100

    rounds order

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -43,7 +43,7 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     @category1 = Category.where(id: @game.category1_id).first
     @category2 = Category.where(id: @game.category2_id).first
-    @colours = [ "card-category-red", "card-category-blue", "card-category-purple", "card-category-yellow" ]
+    @colours = ["card-category-red", "card-category-blue", "card-category-purple", "card-category-yellow" ]
     @rounds = Round.where(game_id: @game.id)
     authorize @game
   end
