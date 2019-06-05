5479d7401add27ea16f42327e53b00abc6527f15
Kieran Doyle <kieranhbdoyle@gmail.com>
2019-06-05 10:43:44 +0100

    added colour column to categories, and small styling improvements on lobby page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_134739) do
+ActiveRecord::Schema.define(version: 2019_06_05_083637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_134739) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
   create_table "games", force: :cascade do |t|
